0be8473195a39613ca91120a349aa03d76e6b0fc
Daniel Simon <DanSimon09@gmail.com>
2022-07-10 10:26:38 -0700

    Updated structs module

--- src/livoxsdk/structs/__init__.py
+++ src/livoxsdk/structs/__init__.py
@@ -1,3 +1,7 @@
-from .structure_type import StructureType
+from .extrinsics import Extrinsics
+from .device_info import DeviceInfo
+from . import point
 from .preamble import Preamble
 from .command_header import CommandHeader
+from .status import StatusUnion, ErrorMessage, HubErrorCode, LidarErrorCode
+from .packet import Packet, PacketHeader
