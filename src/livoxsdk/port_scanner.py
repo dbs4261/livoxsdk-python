34ae29ee113423d9a263173dacdee055545647b1
Daniel Simon <DanSimon09@gmail.com>
2022-07-10 10:33:51 -0700

    Updated port scanner to work with debug mode

--- src/livoxsdk/port_scanner.py
+++ src/livoxsdk/port_scanner.py
@@ -1,31 +1,31 @@
 import asyncio
 import dataclasses
 import datetime
+import ipaddress
 import socket
 import typing
 
-import livoxsdk.enums.messages
-import livoxsdk.enums.devices
-import livoxsdk.structs.packet
-import livoxsdk.crc
-import livoxsdk.payloads
-import livoxsdk.logging_helpers
+import livoxsdk
 
-import logging
+
 logger = livoxsdk.logging_helpers.logger.getChild("PortScanner")
+
+
+class NoDevicesDetectedError(Exception):
+    pass
 
 
 @dataclasses.dataclass(repr=True, eq=True, order=True, unsafe_hash=True)
 class DeviceConnectionInfo:
     serial_number: str
     range_code: int
-    device_type: livoxsdk.enums.devices.DeviceType
-    ip_address: str
-    port: int
+    device_type: livoxsdk.enums.DeviceType
+    ip_address: ipaddress.IPv4Address
+    port: livoxsdk.Port
 
 
 class PortScanner(asyncio.DatagramProtocol):
-    def __init__(self, devices_future: asyncio.Future, port: int = 65000):
+    def __init__(self, devices_future: asyncio.Future, port: livoxsdk.Port = livoxsdk.Port(65000)):
         self.devices_future: asyncio.Future = devices_future
         self.port = port
         self.found_devices: typing.Set[DeviceConnectionInfo] = set()
@@ -33,15 +33,16 @@ class PortScanner(asyncio.DatagramProtocol):
     def datagram_received(self, data: bytes, addr: typing.Tuple[str, int]) -> None:
         logger.getChild("DatagramReceived").debug("{} {}".format(data.hex(), addr))
         if addr[1] == self.port:
-            packet = livoxsdk.structs.Packet.Packet.from_buffer_copy(data)
+            packet = livoxsdk.structs.Packet.from_buffer_copy(data)
             if not packet.valid():
                 raise livoxsdk.crc.CrcChecksumError("Invalid packet encountered during port scan")
             if packet.packet_type == livoxsdk.enums.messages.MessageType.MSG and \
                     packet.command_set == livoxsdk.enums.messages.CommandSet.General and \
                     packet.command_type == livoxsdk.enums.messages.GeneralCommandId.Broadcast:
-                broadcast_payload = livoxsdk.structs.Payloads.BroadcastPayload.from_buffer_copy(packet.payload)
+                broadcast_payload: livoxsdk.payloads.BroadcastPayload = packet.get_payload()
                 device = DeviceConnectionInfo(broadcast_payload.serial, broadcast_payload.range_code,
-                                              broadcast_payload.device_type, addr[0], addr[1])
+                                              broadcast_payload.device_type, ipaddress.IPv4Address(addr[0]),
+                                              livoxsdk.Port(addr[1]))
                 self.found_devices.add(device)
 
     def connection_lost(self, exc: typing.Optional[Exception]) -> None:
@@ -55,17 +56,13 @@ class PortScanner(asyncio.DatagramProtocol):
             raise exc
 
 
-async def scan_for_devices(scan_time: datetime.timedelta, command_port: int = 55000
-                           ) -> typing.Set[DeviceConnectionInfo]:
+async def scan_for_devices(scan_time: datetime.timedelta) -> typing.Set[DeviceConnectionInfo]:
     loop = asyncio.get_running_loop()
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind(("0.0.0.0", command_port))
-
     devices_future = loop.create_future()
     logger.info("Scanning for Livox devices...")
     transport, protocol = await loop.create_datagram_endpoint(
-        lambda: PortScanner(devices_future), sock=server_socket)
+        lambda: PortScanner(devices_future), family=socket.AF_INET, allow_broadcast=True,
+        reuse_port=True, local_addr=("0.0.0.0", livoxsdk.scan_port))
     try:
         await asyncio.sleep(scan_time.total_seconds())
     finally:
@@ -74,8 +71,16 @@ async def scan_for_devices(scan_time: datetime.timedelta, command_port: int = 55
     logger.info("Device scan complete")
     return found_devices
 
+
 if __name__ == '__main__':
+    import logging
+    import sys
+    livoxsdk.logging_helpers.SetupDefaultLogger(None, logging.DEBUG, None)
+    if 'pdb' in sys.modules or 'pydevd' in sys.modules:
+        debug = True
+    else:
+        debug = False
     livoxsdk.logging_helpers.SetupDefaultLogger(None, logging.DEBUG, None, use_simple_console_format=True)
     scan_time = datetime.timedelta(seconds=10)
-    found_devices = asyncio.run(scan_for_devices(scan_time))
+    found_devices = asyncio.run(scan_for_devices(scan_time), debug=debug)
     logger.info("Found Devices:\n" + "\n".join(str(d) for d in found_devices))
