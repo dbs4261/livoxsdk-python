50de64be9b3fa17b3b47a4c4760bc3e3eb91a580
Daniel Simon <DanSimon09@gmail.com>
2022-07-10 07:46:47 -0700

    Changed packet so it is no longer trying to be memory mappable

--- src/livoxsdk/structs/packet.py
+++ src/livoxsdk/structs/packet.py
@@ -1,16 +1,10 @@
 import ctypes
 import typing
 
-import livoxsdk.crc
-
-if __name__ == "__main__":
-    from livoxsdk.structs.structure_type import StructureType
-    from livoxsdk.structs.preamble import Preamble
-    from livoxsdk.structs.command_header import CommandHeader
-else:
-    from .structure_type import StructureType
-    from .preamble import Preamble
-    from .command_header import CommandHeader
+import livoxsdk
+from livoxsdk.structs.structure_type import StructureType
+from livoxsdk.structs.preamble import Preamble
+from livoxsdk.structs.command_header import CommandHeader
 
 
 class PacketHeader(StructureType):
@@ -22,148 +16,141 @@ class PacketHeader(StructureType):
         ("_length", "length"),
     )
     _anonymous_ = (
-        "_preamble",
-        "_command_handler",
+        "preamble",
+        "command_handler",
     )
     _fields_ = (
-        ("_preamble", Preamble),
-        ("_command_handler", CommandHeader),
+        ("preamble", Preamble),
+        ("command_handler", CommandHeader),
     )
     _defaults_ = {
         **getattr(Preamble, "_defaults_", dict())
     }
 
-    packet_type = Preamble.packet_type
-    length = Preamble.length
-    command_set = CommandHeader.command_set
-    command_type = CommandHeader.command_type
+    packet_type: property = Preamble.packet_type
+    length: property = Preamble.length
+    command_set: property = CommandHeader.command_set
+    command_type: property = CommandHeader.command_type
+
+
+class Packet:
+    header: PacketHeader
+    raw_payload: bytearray
+    _packet_crc: ctypes.c_uint32
+
+    def __init__(self, header: PacketHeader,
+                 payload: typing.Union[bytes, bytearray, typing.SupportsBytes] = bytes(),
+                 crc: typing.Optional[int] = None):
+        self.header: PacketHeader = header
+        self.raw_payload: bytearray = bytearray(bytes(payload))
+        self._packet_crc: ctypes.c_uint32
+        if crc is None:
+            self.header.length = self.length
+            self.header.preamble.preamble_crc = ctypes.c_uint16(self.header.preamble.crc())
+            self._packet_crc = ctypes.c_uint32(self.crc())
+        else:
+            self._packet_crc = ctypes.c_uint32(crc)
+
+    def __bytes__(self) -> bytes:
+        return bytes(self.header) + self.raw_payload + bytes(self._packet_crc)
+
+    def __str__(self) -> str:
+        payload = self.get_payload()
+        return "Packet {{{}, payload<{}>: {}, packet_crc: {}}}".format(
+            self.header, type(payload).__name__, payload, self.packet_crc)
 
     @property
     def preamble(self) -> Preamble:
-        return Preamble.from_buffer_copy(self)
+        return self.header.preamble
 
     @preamble.setter
     def preamble(self, val: Preamble) -> None:
-        if not isinstance(val, Preamble):
-            raise TypeError("Val must be {} but instead is {}".format(type(Preamble), type(val)))
-        ctypes.memmove(ctypes.byref(self), ctypes.byref(val), ctypes.sizeof(Preamble))
+        self.header.preamble = val
 
     @property
-    def command_header(self) -> CommandHeader:
-        return CommandHeader.from_buffer_copy(self, ctypes.sizeof(Preamble))
+    def command_handler(self) -> CommandHeader:
+        return self.header.command_handler
 
-    @command_header.setter
-    def command_header(self, val: CommandHeader) -> None:
-        if not isinstance(val, CommandHeader):
-            raise TypeError("Val must be {} but instead is {}".format(type(CommandHeader), type(val)))
-        ctypes.memmove(ctypes.byref(self, ctypes.sizeof(Preamble)), ctypes.byref(val), ctypes.sizeof(Preamble))
+    @command_handler.setter
+    def command_handler(self, val: CommandHeader) -> None:
+        self.header.command_handler = val
 
+    @property
+    def packet_type(self) -> livoxsdk.enums.MessageType:
+        return self.header.packet_type
 
-class Packet(StructureType):
-    _pack_ = 1
-    _mapped_ = (
-        ("packet_type_c", "packet_type"),
-        ("command_set_c", "command_set"),
-        ("command_type_c", "command_type"),
-        ("_payload", "payload"),
-        ("_packet_crc", "packet_crc"),
-        ("_length", "length"),
-    )
-    _anonymous_ = (
-        "_header",
-    )
-    _fields_ = (
-        ("_header", PacketHeader),
-        ("_payload", ctypes.c_byte * 0),
-        ("_packet_crc", ctypes.c_uint32),
-    )
-    _defaults_ = {
-        **getattr(PacketHeader, "_defaults_", dict())
-    }
+    @packet_type.setter
+    def packet_type(self, val: livoxsdk.enums.MessageType) -> None:
+        self.header.packet_type = val
 
-    packet_type = Preamble.packet_type
-    command_set = CommandHeader.command_set
-    command_type = CommandHeader.command_type
-    preamble = PacketHeader.preamble
-    command_header = PacketHeader.command_header
+    @property
+    def command_set(self) -> livoxsdk.enums.CommandSet:
+        return self.header.command_set
 
-    def __init__(self, **kwargs):
-        values = {}
-        values["_length"] = ctypes.sizeof(self)
-        values.update(kwargs)
-        super().__init__(**values)
-        setattr(self, "preamble_crc", Preamble.from_buffer(self).crc())
-        self.packet_crc = self.crc()
+    @property
+    def command_type(self) -> livoxsdk.enums.CommandId:
+        return self.header.command_type
+
+    @command_type.setter
+    def command_type(self, val: livoxsdk.enums.CommandId) -> None:
+        self.header.command_type = val
 
     @property
     def length(self) -> ctypes.c_uint16:
-        return self._length
-
-    @length.setter
-    def length(self, val: int) -> None:
-        if val != ctypes.c_uint16(val).value:
-            raise ValueError("Invalid length {} must be betwee {} and {}".format(
-                val, 0, ctypes.c_uint16(-1).value))
-        if val < ctypes.sizeof(type(self)):
-            raise ValueError("Cannot shrink a Packet to have a smaller size than one without a payload")
-        tmp_crc = self.packet_crc
-        self._length = val
-        ctypes.resize(self, val)
-        self.packet_crc = tmp_crc
+        return ctypes.c_uint16(ctypes.sizeof(self.header) +
+                len(self.raw_payload) + ctypes.sizeof(ctypes.c_uint32))
 
     @property
     def packet_crc(self) -> int:
-        offset = ctypes.sizeof(self) - ctypes.sizeof(ctypes.c_uint32)
-        return ctypes.c_uint32.from_buffer_copy(self, offset).value
+        return self._packet_crc.value
 
     @packet_crc.setter
-    def packet_crc(self, val: typing.Union[int, ctypes.c_uint32]) -> None:
-        if isinstance(val, int):
-            if val != ctypes.c_uint32(val).value:
-                raise ValueError("Invalid packet_crc {} must be betwee {} and {}".format(
-                    val, 0, ctypes.c_uint32(-1).value))
-        if not isinstance(val, ctypes.c_uint32):
-            self.packet_crc = ctypes.c_uint32(val)
-            return
-        offset = ctypes.sizeof(self) - ctypes.sizeof(ctypes.c_uint32)
-        ctypes.memmove(ctypes.byref(self, offset), bytes(val), ctypes.sizeof(ctypes.c_uint32))
+    def packet_crc(self, val: int) -> None:
+        self._packet_crc = ctypes.c_uint32(val)
 
-    def crc(self):
-        return livoxsdk.crc.crc32Func(bytes(self)[:ctypes.sizeof(self) - ctypes.sizeof(ctypes.c_uint32)])
+    def crc(self) -> int:
+        crc32_result = livoxsdk.crc.crc32Func(bytes(self.header) + self.raw_payload)
+        return crc32_result
 
-    @property
-    def payload(self) -> bytes:
-        payload_size = ctypes.sizeof(self) - ctypes.sizeof(type(self))
-        return bytes((ctypes.c_byte * payload_size).from_buffer_copy(
-            self, ctypes.sizeof(Preamble) + ctypes.sizeof(CommandHeader)))
+    def get_payload(self) -> typing.Union[bytes, livoxsdk.BinarySerializable, typing.Any]:
+        if (self.packet_type, self.command_type) in livoxsdk.payloads.payload_mapping:
+            PayloadType: typing.Optional[typing.Type[livoxsdk.BinarySerializable]] = \
+                livoxsdk.payloads.payload_mapping[(self.packet_type, self.command_type)]
+            if PayloadType is not None:
+                return PayloadType.from_buffer_copy(self.raw_payload)
+            else:
+                return bytes(self.raw_payload)
+        else:
+            return bytes(self.raw_payload)
 
-    @payload.setter
-    def payload(self, val: typing.SupportsBytes) -> None:
-        payload_size = len(bytes(val))
-        payload_offset = ctypes.sizeof(Preamble) + ctypes.sizeof(CommandHeader)
-        self.length = ctypes.sizeof(type(self)) + payload_size
-        ctypes.memmove(ctypes.byref(self, payload_offset), bytes(val), payload_size)
-
-    @payload.deleter
-    def payload(self):
-        self.length = ctypes.sizeof(type(self))
+    def set_payload(self, val: typing.SupportsBytes) -> None:
+        self.raw_payload = bytearray(bytes(val))
+        self.packet_crc = ctypes.c_uint32(self.crc())
 
     @classmethod
     def from_buffer_copy(cls, source: typing.Union[bytes, bytearray, memoryview], offset: int = 0) -> "Packet":
-        preamble = Preamble.from_buffer_copy(source, offset)
-        out = cls()
-        ctypes.resize(out, preamble.length)
-        raw_memory = (ctypes.c_byte * len(source)).from_buffer_copy(source, offset)
-        ctypes.memmove(ctypes.byref(out), ctypes.byref(raw_memory, offset), ctypes.sizeof(out))
-        return out
-
-    @classmethod
-    def from_buffer(cls, *args, **kwargs):
-        raise NotImplementedError("From buffer does not work correctly because of the payload packet layout. "
-                                  "Use the PacketHeader class to see a memory view of a packet")
+        header = PacketHeader.from_buffer_copy(source, offset)
+        offset += ctypes.sizeof(PacketHeader)
+        payload_size = header.length - ctypes.sizeof(PacketHeader) - ctypes.sizeof(ctypes.c_uint32)
+        payload = bytes(source[offset:offset+payload_size])
+        offset += payload_size
+        crc = int.from_bytes(source[offset:offset+ctypes.sizeof(ctypes.c_uint32)], livoxsdk.endianness)
+        return Packet(header, payload, crc)
 
     def valid(self) -> typing.Tuple[bool, bool]:
-        return Preamble.from_buffer(self).crc() == getattr(self, "preamble_crc"), self.crc() == self.packet_crc
+        return self.preamble.valid(), self.crc() == self.packet_crc
+
+    def validate(self) -> None:
+        expected_crc16 = self.preamble.preamble_crc
+        calculated_crc16 = self.preamble.crc()
+        expected_crc32 = self.packet_crc
+        calculated_crc32 = self.crc()
+        if expected_crc16 != calculated_crc16:
+            raise livoxsdk.crc.CrcChecksumError(
+                "CRC16 mismatch: calculated {} expected {}".format(calculated_crc16, expected_crc16))
+        if expected_crc32 != calculated_crc32:
+            raise livoxsdk.crc.CrcChecksumError(
+                "CRC32 mismatch: calculated {} expected {}".format(calculated_crc32, expected_crc32))
 
 
 if __name__ == "__main__":
