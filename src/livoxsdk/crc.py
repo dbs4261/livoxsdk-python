c238e81ae90144b0c7602f13d55813791ab8ae32
Daniel Simon <DanSimon09@gmail.com>
2022-07-09 19:39:31 -0700

    Added typing to crc functions

--- src/livoxsdk/crc.py
+++ src/livoxsdk/crc.py
@@ -2,10 +2,12 @@ import crcmod
 import typing
 
 
-crc16Func: typing.Callable[[bytes, int], int] = crcmod.mkCrcFun(
-    0x11021, rev=True, initCrc=0x4C49)
-crc32Func: typing.Callable[[bytes, int], int] = crcmod.mkCrcFun(
-    0x104C11DB7, rev=True, initCrc=0x564F580A, xorOut=0xFFFFFFFF)
+class CrcFuncProtocol(typing.Protocol):
+    def __call__(self, data, crc=...) -> int: ...
+
+
+crc16Func: CrcFuncProtocol = crcmod.mkCrcFun(0x11021, rev=True, initCrc=0x4C49)
+crc32Func: CrcFuncProtocol = crcmod.mkCrcFun(0x104C11DB7, rev=True, initCrc=0x564F580A, xorOut=0xFFFFFFFF)
 
 
 class CrcChecksumError(Exception):
